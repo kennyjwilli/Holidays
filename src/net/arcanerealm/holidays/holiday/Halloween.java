d6e902109fd13422baa0ebc0bf99797599380029
Jeppe Vennekilde <jeppe@jeppes.info>
2013-12-07 06:11:38 +0100

    Updated to 1.7.2

--- src/net/arcanerealm/holidays/holiday/Halloween.java
+++ src/net/arcanerealm/holidays/holiday/Halloween.java
@@ -9,8 +9,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Sound;
-import org.bukkit.craftbukkit.v1_6_R3.entity.CraftBat;
-import org.bukkit.craftbukkit.v1_6_R3.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_7_R1.entity.CraftBat;
+import org.bukkit.craftbukkit.v1_7_R1.entity.CraftPlayer;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
